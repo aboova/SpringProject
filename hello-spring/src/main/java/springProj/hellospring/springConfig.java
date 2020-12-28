4cefb31f2364e10e7f4221dd0229648a94874775
aboova <wowjd1375@naver.com>
2020-12-28 23:11:05 +0900

    JDBC Connect

--- hello-spring/src/main/java/springProj/hellospring/springConfig.java
+++ hello-spring/src/main/java/springProj/hellospring/springConfig.java
@@ -1,14 +1,26 @@
 package springProj.hellospring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springProj.hellospring.repository.JdbcMemberRepository;
 import springProj.hellospring.repository.MemberRepository;
 import springProj.hellospring.repository.MemoryMemberRepository;
 import springProj.hellospring.service.MemberService;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class springConfig {
     //스프링 빈을 등록할 거야!
+
+    private DataSource dataSource;
+
+    @Autowired
+    public springConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
@@ -16,6 +28,7 @@ public class springConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        //return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
